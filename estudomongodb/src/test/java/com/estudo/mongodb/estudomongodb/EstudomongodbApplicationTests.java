1169aa6732f839855ebf541bbc4728bf38e9ea7a
RamiroCyber <ramiro.dr@gmail.com>
2022-06-02 22:57:14 -0300

    iniciando projeto

---
+++ estudomongodb/src/test/java/com/estudo/mongodb/estudomongodb/EstudomongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.mongodb.estudomongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudomongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
