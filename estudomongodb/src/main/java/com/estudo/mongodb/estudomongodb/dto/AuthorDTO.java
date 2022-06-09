7f315398c6d98ab55d8d1a311100c9c584159bd2
RamiroCyber <ramiro.dr@gmail.com>
2022-06-08 23:34:55 -0300

    criando referencias dos posts em user

---
+++ estudomongodb/src/main/java/com/estudo/mongodb/estudomongodb/dto/AuthorDTO.java
@@ -0,0 +1,38 @@
+package com.estudo.mongodb.estudomongodb.dto;
+
+import javax.validation.constraints.NotBlank;
+
+import com.estudo.mongodb.estudomongodb.entities.User;
+
+public class AuthorDTO {
+
+	@NotBlank
+	private String id;
+	@NotBlank
+	private String name;
+
+	public AuthorDTO() {
+	}
+
+	public AuthorDTO(User user) {
+		this.id = user.getId();
+		this.name = user.getName();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
